46f4b749f220fce3ccb9221b3e3ec111b9cdf4b3
Lauren Noeltner <laurennoeltner@gmail.com>
2020-12-01 21:06:08 -0500

    added portfolio images folder with all imgs

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Header from "./Header";
 import { Link, BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import { NavLink } from "react-router-dom";
-import About from "./About";
+// import About from "./About";
 import "../App.css";
 
 
@@ -14,9 +13,22 @@ const Home = () => {
     return (
         <div>
             <Header />
-            <h1 id="homeLauren">Lauren Noeltner</h1>
-            <h3 id="descriptionHome">I am a Full Stack web Developer in Atlanta, GA, always looking for new opportunities to strengthen my skills and create innovative apps</h3>
-            <Link id="viewMoreBtn" to="/About">View More... </Link>
+            <div className="row">
+                <div className="col-12">
+                    <h1 id="homeLauren">Lauren Noeltner</h1>
+                </div>    
+            </div> 
+            <div className="row">
+                <div className="col-12">
+                    <h3 id="descriptionHome">I am a Full Stack web Developer in Atlanta, GA, always looking for new opportunities to strengthen my skills and create innovative apps</h3>
+                </div>   
+            </div> 
+            <div className="row">
+                <div className="viewMoreBtn col-12">
+                    <Link className="viewMore" to="/About">View More... </Link>
+                </div>   
+            </div> 
+            
         </div>
     );
 };
