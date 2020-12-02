1e3db9944aaf4a936f65d5a87a92f69f8b9e81c2
Lauren Noeltner <laurennoeltner@gmail.com>
2020-12-01 19:25:51 -0500

    added routes and tested perfectly

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+// import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
+          <p>Hello World</p>
           <Switch>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/" component={Login} />
-            <Route exact path="/" component={Login} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/About" component={About} />
+            <Route exact path="/Portfolio" component={Portfolio} />
+            <Route exact path="/Contact" component={Contact} />
           </Switch>
         </header>
       </div>
