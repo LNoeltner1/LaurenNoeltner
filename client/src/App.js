7bc75f2f08e5134de91425a917884618d4e246ec
Lauren Noeltner <laurennoeltner@gmail.com>
2020-12-01 19:14:17 -0500

    removed unnecessary files/code; added component files

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
@@ -7,16 +11,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
