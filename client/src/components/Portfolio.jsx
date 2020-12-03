f53b9ab67d35576f011881e1f3db18f26028df1b
Lauren Noeltner <laurennoeltner@gmail.com>
2020-12-02 21:52:06 -0500

    added 6 extra items to the carousel using the template for number 1

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -31,6 +31,7 @@ const Portfolio = () => {
                 <div className="col-xs-0 col-sm-1"></div>
                 <div className="carouselWrapper col-xs-12 col-sm-10">
                     <Carousel>
+                        {/* ITEM 1 */}
                         <Carousel.Item interval={15000}>
                             
                                 <div className="row">
@@ -59,17 +60,179 @@ const Portfolio = () => {
                                 </div>
                                 
                         </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                            className="d-block w-100"
-                            src={Cryptic}
-                            alt="Third slide"
-                            />
-
-                            <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </Carousel.Caption>
+                        {/* ITEM 2 */}
+                        <Carousel.Item interval={15000}>
+                            
+                                <div className="row">
+                                    <div className="col-1"></div>
+                                    <div className="col-10">
+                                        
+                                        <h3 className="titleText">Cryptic Encounters</h3>
+                                        <p className="descriptionText">
+                                            Wild West-themed chore app for young children, who can enlist as 
+                                            'Deputies' to their parents (the 'Sheriffs') and complete extra 
+                                            chores around the house in exchange for points. Points can earn 
+                                            them extra privileges at home, such as a later bedtime, a more 
+                                            screentime, or whatever the parent can think of!
+                                        </p>
+                                    </div> 
+                                    <div className="col-1"></div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <img
+                                        className="carouselItem d-block w-100"
+                                        src={GiddyItUp}
+                                        alt="First slide"
+                                        />
+                                    </div>
+                                </div>
+                                
+                        </Carousel.Item>
+                        {/* ITEM 3 */}
+                        <Carousel.Item interval={15000}>
+                            
+                                <div className="row">
+                                    <div className="col-1"></div>
+                                    <div className="col-10">
+                                        
+                                        <h3 className="titleText">GiddyItUp</h3>
+                                        <p className="descriptionText">
+                                            Wild West-themed chore app for young children, who can enlist as 
+                                            'Deputies' to their parents (the 'Sheriffs') and complete extra 
+                                            chores around the house in exchange for points. Points can earn 
+                                            them extra privileges at home, such as a later bedtime, a more 
+                                            screentime, or whatever the parent can think of!
+                                        </p>
+                                    </div> 
+                                    <div className="col-1"></div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <img
+                                        className="carouselItem d-block w-100"
+                                        src={GiddyItUp}
+                                        alt="First slide"
+                                        />
+                                    </div>
+                                </div>
+                                
+                        </Carousel.Item>
+                        {/* ITEM 4 */}
+                        <Carousel.Item interval={15000}>
+                            
+                                <div className="row">
+                                    <div className="col-1"></div>
+                                    <div className="col-10">
+                                        
+                                        <h3 className="titleText">GiddyItUp</h3>
+                                        <p className="descriptionText">
+                                            Wild West-themed chore app for young children, who can enlist as 
+                                            'Deputies' to their parents (the 'Sheriffs') and complete extra 
+                                            chores around the house in exchange for points. Points can earn 
+                                            them extra privileges at home, such as a later bedtime, a more 
+                                            screentime, or whatever the parent can think of!
+                                        </p>
+                                    </div> 
+                                    <div className="col-1"></div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <img
+                                        className="carouselItem d-block w-100"
+                                        src={GiddyItUp}
+                                        alt="First slide"
+                                        />
+                                    </div>
+                                </div>
+                                
+                        </Carousel.Item>
+                        {/* ITEM 5 */}
+                        <Carousel.Item interval={15000}>
+                            
+                                <div className="row">
+                                    <div className="col-1"></div>
+                                    <div className="col-10">
+                                        
+                                        <h3 className="titleText">GiddyItUp</h3>
+                                        <p className="descriptionText">
+                                            Wild West-themed chore app for young children, who can enlist as 
+                                            'Deputies' to their parents (the 'Sheriffs') and complete extra 
+                                            chores around the house in exchange for points. Points can earn 
+                                            them extra privileges at home, such as a later bedtime, a more 
+                                            screentime, or whatever the parent can think of!
+                                        </p>
+                                    </div> 
+                                    <div className="col-1"></div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <img
+                                        className="carouselItem d-block w-100"
+                                        src={GiddyItUp}
+                                        alt="First slide"
+                                        />
+                                    </div>
+                                </div>
+                                
+                        </Carousel.Item>
+                        {/* ITEM 6 */}
+                        <Carousel.Item interval={15000}>
+                            
+                                <div className="row">
+                                    <div className="col-1"></div>
+                                    <div className="col-10">
+                                        
+                                        <h3 className="titleText">GiddyItUp</h3>
+                                        <p className="descriptionText">
+                                            Wild West-themed chore app for young children, who can enlist as 
+                                            'Deputies' to their parents (the 'Sheriffs') and complete extra 
+                                            chores around the house in exchange for points. Points can earn 
+                                            them extra privileges at home, such as a later bedtime, a more 
+                                            screentime, or whatever the parent can think of!
+                                        </p>
+                                    </div> 
+                                    <div className="col-1"></div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <img
+                                        className="carouselItem d-block w-100"
+                                        src={GiddyItUp}
+                                        alt="First slide"
+                                        />
+                                    </div>
+                                </div>
+                                
+                        </Carousel.Item>
+                        {/* ITEM 7 */}
+                        <Carousel.Item interval={15000}>
+                            
+                                <div className="row">
+                                    <div className="col-1"></div>
+                                    <div className="col-10">
+                                        
+                                        <h3 className="titleText">GiddyItUp</h3>
+                                        <p className="descriptionText">
+                                            Wild West-themed chore app for young children, who can enlist as 
+                                            'Deputies' to their parents (the 'Sheriffs') and complete extra 
+                                            chores around the house in exchange for points. Points can earn 
+                                            them extra privileges at home, such as a later bedtime, a more 
+                                            screentime, or whatever the parent can think of!
+                                        </p>
+                                    </div> 
+                                    <div className="col-1"></div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <img
+                                        className="carouselItem d-block w-100"
+                                        src={GiddyItUp}
+                                        alt="First slide"
+                                        />
+                                    </div>
+                                </div>
+                                
                         </Carousel.Item>
                 
 
