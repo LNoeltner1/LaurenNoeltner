616c3fa771f4435512de029dac3ac1cc855f227b
Lauren Noeltner <laurennoeltner@gmail.com>
2020-12-07 23:01:27 -0500

    finished six portfolio project cards

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -41,6 +41,7 @@ const Portfolio = () => {
                     <div className="carouselWrapper col-xs-12 col-sm-10">
                         
                     <CardColumns>
+                        {/* CARD ONE */}
                         <Card className="individualCard">
                             <Card.Img variant="top" src={GiddyItUp} className="cardImage"/>
                             <Card.Body>
@@ -56,6 +57,83 @@ const Portfolio = () => {
                             <a className="liveLink" href="https://giddyitup-eg.herokuapp.com/">Visit Live Project!</a>
                             </Card.Body>
                         </Card>
+                        {/* CARD TWO */}
+                        <Card className="individualCard">
+                            <Card.Img variant="top" src={Workout} className="cardImage"/>
+                            <Card.Body>
+                            <Card.Title className="titleText">Cryptic Encounters</Card.Title>
+                            <Card.Text className="descriptionText">
+                                Workout tracker app with a MySQL database to save their workouts. 
+                                The user can input workouts by type and other factors such as time 
+                                and distance, and view several graphical summaries of their workouts 
+                                over time. 
+                            </Card.Text>
+                            <a className="repoLink" href="https://github.com/LNoeltner1/workout_tracker">GitHub Repository</a>
+                            <a className="liveLink" href="https://workout-tracker-3.herokuapp.com/">Visit Live Project!</a>
+                            </Card.Body>
+                        </Card>
+                        {/* CARD THREE */}
+                        <Card className="individualCard">
+                            <Card.Img variant="top" src={Cryptic} className="cardImage"/>
+                            <Card.Body>
+                            <Card.Title className="titleText">Cryptic Encounters</Card.Title>
+                            <Card.Text className="descriptionText">
+                            A delightfully spooky haunted house walk through game with interactive dialogue 
+                                and varying content. The player finds themselves trapped in 
+                                a haunted house, populated by a group of not-so-mean ghosts.
+                                They must interact with all of these ghosts to escape the house, 
+                                and have the option of choosing positive or negative interactions
+                                with corresponding responses from the spirits.
+                                Made using a persona generator API, a backend 
+                                MongoDB database and Handlebars.
+                            </Card.Text>
+                            <a className="repoLink" href="https://github.com/LNoeltner1/cryptic-encounters">GitHub Repository</a>
+                            <a className="liveLink" href="https://cryptic-encounter.herokuapp.com">Visit Live Project!</a>
+                            </Card.Body>
+                        </Card>
+                        {/* CARD FOUR */}
+                        <Card className="individualCard">
+                            <Card.Img variant="top" src={TeamGenerator} className="cardImage"/>
+                            <Card.Body>
+                            <Card.Title className="titleText">Employee Summary Template Generator CLI</Card.Title>
+                            <Card.Text className="descriptionText">
+                                A CLI app for generating a page that shows all employees in an organized 
+                                way. The user can input name, title and other factors depending on their title.
+                            </Card.Text>
+                            <a className="repoLink" href="https://github.com/LNoeltner1/Employee_Summary_Template_Engine">GitHub Repository</a>
+                            </Card.Body>
+                        </Card>
+                        {/* CARD FIVE */}
+                        <Card className="individualCard">
+                            <Card.Img variant="top" src={Budget} className="cardImage"/>
+                            <Card.Body>
+                            <Card.Title className="titleText">Budget Tracker</Card.Title>
+                            <Card.Text className="descriptionText">
+                                Online/offline budget tracker using a 
+                                local database to store user information over sessions.
+                                The user can input and view a list of expenses and spending over time, 
+                                and can even view a graph of their spending over the lifespan of the local database.
+                            </Card.Text>
+                            <a className="repoLink" href="https://github.com/LNoeltner1/budget-tracker">GitHub Repository</a>
+                            <a className="liveLink" href="https://budget-tracker-lnoeltner.herokuapp.com/">Visit Live Project!</a>
+                            </Card.Body>
+                        </Card>
+                        {/* CARD SIX */}
+                        <Card className="individualCard">
+                            <Card.Img variant="top" src={Password} className="cardImage"/>
+                            <Card.Body>
+                            <Card.Title className="titleText">Random Password Generator</Card.Title>
+                            <Card.Text className="descriptionText">
+                                A random password generator app with the option to use or 
+                                not use different character types. The user can specify the 
+                                length and the types of characters they want to include in their password.
+                            </Card.Text>
+                            <a className="repoLink" href="https://github.com/LNoeltner1/Random_Password_Generator_App">GitHub Repository</a>
+                            <a className="liveLink" href="https://lnoeltner1.github.io/Random_Password_Generator_App/">Visit Live Project!</a>
+                            </Card.Body>
+                        </Card>
+                        {/* CARD SEVEN */}
+                        
                     </CardColumns>
                     </div>
                     <div className="col-xs-0 col-sm-1"></div>
