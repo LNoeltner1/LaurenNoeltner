bd045d5346efed950b7c33a444b292c0321c949a
Lauren Noeltner <laurennoeltner@gmail.com>
2021-04-01 20:13:38 -0400

    removed formating from contact page buttons, reformated for better responsiveness

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -18,20 +18,24 @@ const Contact = () => {
                     <div className="col-1"></div>
                 </div>
                 <div className="row">
-                    <div className="githubLink col-12">
-                        <h2> 
-                            <a className="contactLinks" href="https://github.com/LNoeltner1">Contact Me on GitHub</a>
-                        </h2>
+                    <div className="col-12">
+                        <div className="githubLink">
+                            <h2> 
+                                <a className="contactLinks" href="https://github.com/LNoeltner1">Contact Me on GitHub</a>
+                            </h2>
+                        </div>
+                        <div className="linkedinLink">
+                            <h2> 
+                                <a className="contactLinks" href="https://www.linkedin.com/in/lauren-noeltner-102b731a0">Contact Me on LinkedIn</a>
+                            </h2>
+                        </div>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="linkedinLink col-12">
-                        <h2> 
-                            <a className="contactLinks" href="https://www.linkedin.com/in/lauren-noeltner-102b731a0">Contact Me on LinkedIn</a>
-                        </h2>
-                    </div>
+                    
                 </div>
-                
+                {/* add CODEPEN, HACKERRANK */}
+                {/* Add CONTACT FORM */}
             <Footer />
         </div>
     );
