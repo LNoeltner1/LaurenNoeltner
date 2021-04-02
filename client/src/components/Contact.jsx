e5fabbef2bc60e17cf27cff723d4f3fd464bbdac
Lauren Noeltner <laurennoeltner@gmail.com>
2021-04-01 20:27:31 -0400

    removed formatting from contact page and reformatted for better responsiveness

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -19,15 +19,10 @@ const Contact = () => {
                 </div>
                 <div className="row">
                     <div className="col-12">
-                        <div className="githubLink">
-                            <h2> 
-                                <a className="contactLinks" href="https://github.com/LNoeltner1">Contact Me on GitHub</a>
-                            </h2>
-                        </div>
-                        <div className="linkedinLink">
-                            <h2> 
-                                <a className="contactLinks" href="https://www.linkedin.com/in/lauren-noeltner-102b731a0">Contact Me on LinkedIn</a>
-                            </h2>
+                        <div className="contactLinks">
+                            
+                            <a className="githubLink" href="https://github.com/LNoeltner1">Contact Me on GitHub</a>
+                            <a className="linkedinLink" href="https://www.linkedin.com/in/lauren-noeltner-102b731a0">Contact Me on LinkedIn</a>
                         </div>
                     </div>
                 </div>
